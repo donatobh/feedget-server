b2908da8d2e51c65121f30257070c72d9c6dae99
donatobh <dieimes@yahoo.com>
2022-05-26 20:07:17 -0300

    Changing to Sendgrid

--- src/adapters/nodemailer/nodemailer-mail-adapter.ts
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 import { MailAdapter, SendMailData } from "../mail-adapter";
 
 const transport = nodemailer.createTransport({
-  host: "smtp.mailgun.org",
+  host: "smtp.sendgrid.net",
   port: 587,
   auth: {
     user: process.env.SMTP_USERNAME,
@@ -13,8 +13,8 @@ const transport = nodemailer.createTransport({
 export class NodemailerMailAdapter implements MailAdapter {
   async sendMail({subject, body}: SendMailData) {
     await transport.sendMail({
-      from: 'Equipe Feedget<postmaster@sandbox3c4c9d6a06fc42a1b4e96bd67c70531b.mailgun.org>',
-      to: 'Dieimes Donato <dieimes@yahoo.com.com>',
+      from: 'Equipe Feedget<donato@donatobh.com.br>',
+      to: 'Dieimes Donato <donatobh@gmail.com>',
       subject,
       html: body,
     })
